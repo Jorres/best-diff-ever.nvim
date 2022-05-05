4cc53392f699169d70b83bc8a9250481c97fea56
Jorres' Pebble <jorres.tarasov@gmail.com>
2022-05-05 16:32:52 +0300

    feat: very simple demo, but it works

--- lua/best-diff-ever/init.lua
+++ lua/best-diff-ever/init.lua
@@ -1,10 +1,9 @@
 local uv = require("luv")
+local ns = -1
 
 M = {}
 
-M.start_highlight = function ()
-  print("I am highlight")
-
+M.communicate = function ()
   Stdin =  vim.loop.new_pipe(false)
   Stdout = vim.loop.new_pipe(false)
   Stderr = vim.loop.new_pipe(false)
@@ -41,9 +40,46 @@ M.start_highlight = function ()
   end)
 end
 
+M.start_highlight = function ()
+  vim.api.nvim_buf_add_highlight(0, ns, "MyBestEver", 3, 3, 5)
+  vim.api.nvim_buf_add_highlight(0, ns, "MyBestEver", 5, 3, 5)
+end
+
+M.clear_highlight = function ()
+  vim.api.nvim_exec("hi clear MyBestEver", true)
+  vim.api.nvim_buf_clear_namespace(0, ns, 0, -1)
+end
+
+M.setup = function ()
+  local hi_out = vim.api.nvim_exec("hi MyBestEver guifg=#F00000 guibg=#00F000", true)
+  ns = vim.api.nvim_create_namespace("best-diff-ever-highlight")
+end
+
+-- create_win
+-- get current file path
+-- local filepath = vim.api.nvim_exec("echo @%", true)
 return M
 
+-- ([30:15-20:[INSERT|MOVE]], [30:15-20:[INSERT|MOVE]])
+--
+--
+-- local tbl = {}
+-- table.insert(tbl, "myvalue")
+-- tbl['key'] = "keyvalue"
+-- -- P(tbl)
+-- -- metatables
 
+-- for index, value in ipairs(tbl) do
+--   print(index, value)
+-- end
 
+-- for key, value in pairs(tbl) do
+--   print(key, value)
+-- end
 
--- -- ([30:15-20:[INSERT|MOVE]], [30:15-20:[INSERT|MOVE]])
+-- -- lua - interpreted, weak typing
+-- -- indexing from 1
+-- --
+-- local a = 5
+-- a = "abc"
+-- print(a)
